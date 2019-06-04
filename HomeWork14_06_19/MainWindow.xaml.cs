714632e6fd5a36b6496910c74870b21c2a464a64
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-04 21:09:36 +0600

    HomeWork14.06.19

--- HomeWork14_06_19/MainWindow.xaml.cs
+++ HomeWork14_06_19/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,42 @@ namespace HomeWork14_06_19
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread backgroundMusicThread;
+        private Thread fileSaveThread;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Closed += MainWindowClosed;
+
+            backgroundMusicMediaElement.MediaEnded += BackgroundMusicMediaElementMediaEnded;
+            backgroundMusicMediaElement.Source = new Uri($@"{AppDomain.CurrentDomain.BaseDirectory}\music.mp3");
+
+            fileSaveThread = new Thread(SaveTextToFile) { IsBackground = false };
+
+            backgroundMusicThread = new Thread(backgroundMusicMediaElement.Play) { IsBackground = true };
+            backgroundMusicThread.Start();
+        }
+
+        private void BackgroundMusicMediaElementMediaEnded(object sender, RoutedEventArgs e)
+        {
+            backgroundMusicThread.Start();
+        }
+
+        private void MainWindowClosed(object sender, EventArgs e)
+        {
+            fileSaveThread.Start();
+        }
+
+        private void SaveTextToFile()
+        {
+            string text = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text;
+
+            using (var streamWriter = new StreamWriter(File.Open("Text.txt", FileMode.Create)))
+            {
+                streamWriter.Write(text);
+            }
         }
     }
 }
